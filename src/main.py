bd961160886954be7fcc39204e5b27db05319799
Assylken <asylken02@gmail.com>
2021-10-01 01:26:45 +0600

    first commit, and I hope the last one

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from package_crypto import Pars
+scrapper = Pars()
+def operation(title):
+	scrapper.getInfo(title) 
---
+++ test/test.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.path.insert(0, '/Users/asylk/Desktop/Coinmarketcap/src')
+  
+from main import operation
+name = input("Please enter the name of the Cryptocurrency: ")
+operation(name) 
